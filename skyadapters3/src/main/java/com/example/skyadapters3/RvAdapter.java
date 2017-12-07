b993058621744145db5c27a388aee2649b48bfba
Danilo <danilot.89@gmail.com>
2017-12-07 14:28:02 +0100

    second commit

---
+++ skyadapters3/src/main/java/com/example/skyadapters3/RvAdapter.java
@@ -0,0 +1,77 @@
+package com.example.skyadapters3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class RvAdapter extends RecyclerView.Adapter<RvAdapter.ViewHolder> {
+
+    private Activity a;
+    private int customLayoutID;
+    private Class[] activities;
+    private int color;
+
+    private Menu menu;
+
+
+    public RvAdapter(final Activity a, Menu menu, Class[] activities, int customLayoutID,
+                     RecyclerView.LayoutManager layoutManager, int color) {
+        this.a              = a;
+        this.menu           = menu;
+        this.activities     = activities;
+        this.customLayoutID = customLayoutID;
+        this.color          = color;
+
+        RecyclerView rv = (RecyclerView) a.findViewById(R.id.sky_rv_drawer);
+        rv.setLayoutManager(layoutManager);
+        rv.setAdapter(this);
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txt;
+        ImageView img;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            txt = (TextView) itemView.findViewById(R.id.sky_txt_drawer);
+            img = (ImageView) itemView.findViewById(R.id.sky_img_drawer);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(a).inflate(customLayoutID, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        final MenuItem menuItem = menu.getItem(position);
+        holder.txt.setText(menuItem.getTitle());
+        if (holder.img != null) holder.img.setImageDrawable(menuItem.getIcon());
+
+        holder.itemView.setBackgroundColor(color);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (activities[holder.getAdapterPosition()].getSimpleName().contains("Setting")) {
+                    a.startActivityForResult(new Intent(a, activities[holder.getAdapterPosition()]), 0);
+                } else {
+                    a.startActivity(new Intent(a, activities[holder.getAdapterPosition()]));
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return menu.size();
+    }
+}
---
+++ skyadapters3/src/main/java/com/example/skyadapters3/ToolbarAdapter.java
@@ -0,0 +1,76 @@
+package com.example.skyadapters3;
+
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+
+
+/**
+ * Created by ttlnisoffice on 11/17/17.
+ */
+public class ToolbarAdapter {
+
+    private AppCompatActivity a;
+    private RvAdapter rvAdapter;
+    private Toolbar toolbar;
+
+    public ToolbarAdapter(AppCompatActivity a) {
+        this.a = a;
+    }
+
+    public ToolbarAdapter buildToolbar(int id) {
+        toolbar = (Toolbar) a.findViewById(id);
+        a.setSupportActionBar(toolbar);
+        a.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a.finish();
+            }
+        });
+        return this;
+    }
+
+    public ActionBarDrawerToggle buildToolbarForMainActivity(Class[] activitiesToLaunch,
+                                                             int menuID,
+                                                             int customLayoutID,
+                                                             RecyclerView.LayoutManager layoutManager,
+                                                             int drawerItemColor){
+
+        Menu menu = new PopupMenu(a, null).getMenu();
+        a.getMenuInflater().inflate(menuID, menu);
+
+        rvAdapter = new RvAdapter(a, menu, activitiesToLaunch, customLayoutID, layoutManager, drawerItemColor);
+
+        DrawerLayout drawerLayout = (DrawerLayout) a.findViewById(R.id.sky_drawer_layout);
+        ActionBarDrawerToggle toggleBtn = new ActionBarDrawerToggle(a, drawerLayout,
+                R.string.drawer_open, R.string.drawer_closed);
+        drawerLayout.addDrawerListener(toggleBtn);
+
+        Toolbar toolbar = (Toolbar) a.findViewById(R.id.sky_toolbar);
+        a.setSupportActionBar(toolbar);
+        a.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        toggleBtn.syncState();
+
+        return toggleBtn;
+    }
+
+    public ToolbarAdapter setToolbarTitle(String title){
+        a.getSupportActionBar().setTitle(title);
+        return this;
+    }
+
+    public ToolbarAdapter setToolbarColor(int color) {
+        toolbar.setBackgroundColor(color);
+        return this;
+    }
+}
+

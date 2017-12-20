43507f7b5a3c37aeb6070a7f2b6c9d30292da9b8
Danilo <danilot.89@gmail.com>
2017-12-20 11:12:55 +0100

    message

--- skyadapters3/src/main/java/com/example/skyadapters3/recycler/RvAdapter.java
+++ skyadapters3/src/main/java/com/example/skyadapters3/recycler/RvAdapter.java
@@ -4,29 +4,34 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 /**
  * Created by ttlnisoffice on 12/19/17.
  */
 
-public class RvAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class RvAdapter extends RecyclerView.Adapter<RvHolder> {
 
     private int rvSize;
-    private RecyclerView.ViewHolder holder;
-    RvInterface rvInterface;
+    ArrayList<Integer> ids;
+    private int customRow;
+    private RvInterface rvInterface;
 
-    public RvAdapter(int rvSize, RecyclerView.ViewHolder holder, RvInterface rvInterface) {
+    public RvAdapter(int rvSize, ArrayList<Integer> ids, int customRow, RvInterface rvInterface) {
         this.rvSize = rvSize;
-        this.holder = holder;
+        this.ids = ids;
+        this.customRow = customRow;
         this.rvInterface = rvInterface;
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return holder;
+    public RvHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        return new RvHolder(inflater.inflate(customRow, parent, false), ids);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(RvHolder holder, int position) {
         rvInterface.onBindViewHolder(holder, position);
     }
 
--- skyadapters3/src/main/java/com/example/skyadapters3/recycler/RvHolder.java
+++ skyadapters3/src/main/java/com/example/skyadapters3/recycler/RvHolder.java
@@ -3,12 +3,20 @@ package com.example.skyadapters3.recycler;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import java.util.ArrayList;
+
 /**
  * Created by ttlnisoffice on 12/19/17.
  */
 
 public class RvHolder extends RecyclerView.ViewHolder {
-    public RvHolder(View itemView) {
+
+    View[] views;
+
+    public RvHolder(View itemView, ArrayList<Integer> ids) {
         super(itemView);
+        for (int i = 0; i < ids.size(); i++) {
+            views[i] = itemView.findViewById(ids.get(i));
+        }
     }
 }

f8adaaccc6ae465f3b73ed22e0ec2a4aaae3e5e6
Danilo <danilot.89@gmail.com>
2018-01-03 12:28:41 +0100

    commit

--- skyadapters3/src/main/java/com/example/skyadapters3/recycler/RvActivityWithNavDrawer.java
+++ skyadapters3/src/main/java/com/example/skyadapters3/recycler/RvActivityWithNavDrawer.java
@@ -39,20 +39,20 @@ public abstract class RvActivityWithNavDrawer extends RvBase implements RxBackgr
             if (vg.getChildAt(i) instanceof DrawerLayout) {
                 drawerLayout = (DrawerLayout) findViewById(vg.getChildAt(i).getId());
             }
-            if (vg.getChildAt(i) instanceof LinearLayout) {
-                LinearLayout linearLayout = (LinearLayout) findViewById(vg.getChildAt(i).getId());
-                for (int j = 0; j < linearLayout.getChildCount(); j++) {
-                    if (linearLayout.getChildAt(j) instanceof DrawerLayout) {
-                        drawerLayout = (DrawerLayout) findViewById(vg.getChildAt(j).getId());
-                    }
-                }
-            }
         }
 
         ArrayList<Integer> rvs = new ArrayList<>();
         for (int i = 0; i < drawerLayout.getChildCount(); i++) {
             if (drawerLayout.getChildAt(i) instanceof RecyclerView) {
                 rvs.add(drawerLayout.getChildAt(i).getId());
+            }
+            if (vg.getChildAt(i) instanceof LinearLayout) {
+                LinearLayout linearLayout = (LinearLayout) findViewById(vg.getChildAt(i).getId());
+                for (int j = 0; j < linearLayout.getChildCount(); j++) {
+                    if (linearLayout.getChildAt(j) instanceof RecyclerView) {
+                        rvs.add(drawerLayout.getChildAt(i).getId());
+                    }
+                }
             }
         }
 
